439885062c30cf1d5a608a985e1ea0ff0c613828
Julio Cesar <119695508+JulioCCF@users.noreply.github.com>
2024-02-18 14:32:21 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 function Home() {
   return <h1>Paula , eu te amo!!! se você me ama dá uma risadinha!!</h1>;
 }
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identaçao errada");
-}
 
 export default Home;
